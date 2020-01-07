ec07d03393f0522da1e8eb0dfef08a2c2b50aeb9
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-01-07 10:43:02 +0100

    update to core which uses databases + remove filter app

--- create_figure.py
+++ create_figure.py
@@ -3,12 +3,12 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 import sys
-import pjr.DictHelper as dh
-import pjr.DictRelator as dr
+import pjr.DBHelper as dh
+import pjr.DBRelator as dr
 import argparse
 import common.parser as cp
 import common.properties as prop
-import json
+import numpy as np
 
 parser = argparse.ArgumentParser(parents=[cp.get_common()])
 parser.add_argument('-l', '--list-cases', type=str, help='list of attributes to compare against the case of interest', action='append', dest='list_cases', required=True)
@@ -18,29 +18,28 @@ parser.add_argument('-o', type=str, help='Name of the output figure file', dest=
 args = parser.parse_args()
 
 filter_dict = dh.convert_filter_list_to_dic(args.filter_list)
-input_res = dh.read_json_file(args.input, filter_dict, "val", prop.CASE_INFO, prop.VALUE_INFO)
+case_info = list(args.list_cases)
+case_info.append(args.coi)
+input_res = dh.read_json_file(args.input, filter_dict, case_info, [args.voi])
 
 for i in prop.CASE_INFO:
   print(i, dh.extract_set(input_res, i))
 
-m, coi_set = dr.matrix_relation(input_res, args.list_cases, args.coi, args.voi)
+m, coi_set = dr.matrix_relation(input_res, args.list_cases, args.coi, args.voi, 'auto', 'min')
 coi_set = sorted(coi_set, key=float)
 
 fig = plt.figure()
 ax = fig.gca()
 
 for k, v in m.items():
-  nv = dict()
-  for kv in coi_set:
-    if kv in v:
-      nv[kv] = v[kv].get_min()
-    else:
-      nv[kv] = None
-  ax.plot(nv.keys(), nv.values(), label=str(list(json.loads(k.replace("'", '"')).values())), marker='*')
+  print()
+  print(k, v)
+  ax.plot(np.arange(len(v.keys())), v.values(), label=str(k).replace("'",''), marker='*')
 
 ax.set_ylabel("Time")
 ax.set_xlabel(args.coi)
-ax.set_xticks(coi_set)
+ax.xaxis.set_ticklabels(coi_set)
+ax.xaxis.set_ticks(np.arange(len(coi_set)))
 
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.savefig(args.output, bbox_inches="tight")
