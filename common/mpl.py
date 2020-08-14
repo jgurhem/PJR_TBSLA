e6aee471db6f4d143804e6369a6a05e6150a4003
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-08-14 16:45:15 +0200

    Show a SQL query that can retrieve all the contributions of the points in the plots

--- common/mpl.py
+++ common/mpl.py
@@ -11,7 +11,11 @@ def plot_axis(m, coi_set, attribute, xlabel, ylabel, pv):
   for k in sorted(m.keys()):
     v = m[k]
     if pv:
-      print(k, {k2:round(v2, 4) if isinstance(v2, float) else v2 for k2,v2 in v.items()})
+      print()
+      print()
+      print(k)
+      for k2,v2 in v.items():
+        print(k2, " :: ", round(v2, 4) if isinstance(v2, float) else v2)
     ax.plot(xvec, [v[str(x) + '_' + attribute] for x in coi_set], label=str(k).replace("'",''), marker='*')
 
   ax.set_ylabel(ylabel)
