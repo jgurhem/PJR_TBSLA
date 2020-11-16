47951e3f12ac6109d3197ea05872098190e9dfd4
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-11-16 18:42:44 +0100

    Add the number of cases from which the best case is selected in the latex table

--- common/latex.py
+++ common/latex.py
@@ -5,27 +5,30 @@ import re
 def table(m, filename, legend):
   row_keys = set()
   column_keys = set()
-  N_key = ''
+  Nval_key = ''
+  Ncase_key = ''
   for v in m.values():
     for k , v in v.items():
       row_keys.add(k)
       for i in v.keys():
         if not i.startswith('__'):
           column_keys.add(i)
-        if i.startswith('__') and i.endswith('.N'):
-          N_key = i
+        if i.startswith('__') and i.endswith('.Nval'):
+          Nval_key = i
+        if i.startswith('__') and i.endswith('.Ncase'):
+          Ncase_key = i
   row_keys = sorted(row_keys)
   column_keys = sorted(column_keys)
 
   r = ''
   r += '\\begin{tabular}{'
-  r += 'c' * (len(column_keys) + 2)
+  r += 'c' * (len(column_keys) + 3)
   r += '}\n\\hline\n'
 
   r += 'Cases & Nodes'
   for i in column_keys:
     r += f'& {i}'
-  r += ' & N \\\\'
+  r += ' & N & Ncase \\\\'
   r += '\n\\hline\n'
 
   for k in sorted(m.keys(), key = lambda x:[int(s) if s.isdigit() else s for s in re.split(r'(\d+)', x)]):
@@ -36,7 +39,8 @@ def table(m, filename, legend):
       r += '& ' + str(kr)
       for kc in column_keys:
         r += '& ' + str(round(v[kr][kc], 4) if isinstance(v[kr][kc], float) else v[kr][kc])
-      r += '& ' + str(v[kr].get(N_key, 0))
+      r += '& ' + str(v[kr].get(Nval_key, 0))
+      r += '& ' + str(v[kr].get(Ncase_key, 0))
       r += '\\\\\n'
     r += '\\hline\n'
 
